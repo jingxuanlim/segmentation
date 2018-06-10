05c13fcdf2126ce7e29585465747140bed0fd144
kawashimat <kawashimat@janelia.hhmi.org>
2018-06-10 00:57:06 -0400

    added option for brief stimulus

--- BehaveAndScanJing/struct_def.cs
+++ BehaveAndScanJing/struct_def.cs
@@ -46,6 +46,8 @@ namespace BehaveAndScanSPIM
         public double flash_iti;
         public int flash_col1;
         public int flash_col2;
+        public bool brief;
+        public bool jitter;
 
     }
 
